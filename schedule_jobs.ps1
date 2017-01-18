2f7f7cc2e64fc0c134882af1dd64884d9705ae1e
Leandro Silva <ldfsilva@gmail.com>
2017-01-18 17:16:05 -0600

    Add shedule job functionality

--- schedule_jobs.ps1
+++ schedule_jobs.ps1
@@ -8,6 +8,16 @@ function disable_job($job_name){
     }
 }
 
+# Enable a given job schedule
+function enable_job($job_name){
+    $job = Get-VBRJob -Name $job_name
+    if ($job){
+        Enable-VBRJobSchedule -Job $job
+    }else {
+        echo "Unable to find $job_name"
+    }
+}
+
 # Reset a given job schedule to its default
 function reset_job($job_name){
     $job = Get-VBRJob -Name $job_name
@@ -29,5 +39,55 @@ function disable_all_jobs($job_list){
 function reset_all_jobs($job_list){
     foreach ( $job_name in $job_list ){
         reset_job($job_name)
+    }
+}
+
+# Given an array containing job names, it will iterate through and shedule all of them
+# according to the specified offset.
+function schedule_jobs($job_list){
+    $cnt = 0
+    $off_set = 2
+    $index = 0
+    foreach ( $job_name in $job_list ){
+        $job = Get-VBRJob -Name $job_name
+        echo "cnt: $cnt - off_set: $off_set - index: $index - jobname: $job_name"
+        echo ""
+        if ($job){
+            if ($cnt -lt $off_set){
+                echo "ENTROU"
+                $cnt = $cnt + 1
+                reset_job($job_name)
+                disable_job($job_name)
+                continue
+            }
+            $index = $cnt - $off_set
+            $after_job = $job_list[$index]
+            Set-VBRJobSchedule -Job $job -After -AfterJob $after_job
+            Enable-VBRJobSchedule -Job $job
+        }else {
+            echo "Unable to find $job_name"
+        }
+        $cnt = $cnt + 1
+    }
+}
+
+# Preview how given jobs will look like after scheduling
+function preview_schedule_jobs($job_list){
+    $cnt = -1
+    $off_set = 2
+    $index = 0
+    foreach ( $job_name in $job_list ){
+        $cnt = $cnt + 1
+        if ($cnt -lt $off_set){
+            echo "index: $cnt - job: $job_name"
+            echo "----"
+            continue
+        }
+        $index = $cnt - $off_set
+        $after_job = $job_list[$index]
+
+        echo "index: $cnt - job: $job_name - after - $after_job - index: $index"
+        echo "----"
+
     }
 }
