28a8c9714b511d87e823acdc993000d101b96812
Leandro Silva <ldfsilva@gmail.com>
2017-10-31 16:04:04 -0500

    add bat script for automated cygwin installation with common packages

---
+++ cygwin/cygwin_install.bat
@@ -0,0 +1,49 @@
+C:\cygwin64\setup-x86_64 ^
+--quiet-mode ^
+--disable-buggy-antivirus ^
+-B ^
+-s http://mirror.calvin.edu/cygwin ^
+--packages ^
+autossh,^
+base-cygwin,^
+bind-utils,^
+bc,^
+corkscrew,^
+curl,^
+dos2unix,^
+genisoimage,^
+git,^
+ipcalc,^
+iperf,^
+lynx,^
+man-pages-posix,^
+mkisofs,^
+nc,^
+nc6,^
+openssh,^
+openssl,^
+ping,^
+python,^
+python-httlplib2,^
+python-jinja2,^
+python-openssl,^
+python-paramiko,^
+python-requests,^
+python3,^
+python3-openssl,^
+python3-requests,^
+python3-urllib3,^
+rsync,^
+sharutils,^
+sqlite3,^
+shutdown,^
+stunnel,^
+time,^
+tree,^
+unzip,^
+vim,^
+wdiff,^
+wget,^
+whois,^
+zip,^
+zsync,^
